f7c9cd2c0638e52a0429d24f03f9abca75c426d6
iremkalsin <73405438+iremkalsin@users.noreply.github.com>
2022-12-18 21:09:07 +0300

    Added sql create table script

---
+++ create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE User (
+  UserId int NOT NULL AUTO_INCREMENT,
+  FirstName varchar(255) NOT NULL,
+  LastName varchar(255) NOT NULL,
+  Password varchar(255) NOT NULL,
+  IdentityNumber char(11) NOT NULL,
+  PRIMARY KEY (UserId)
+);
+
+CREATE TABLE Account (
+  AccoundId int NOT NULL AUTO_INCREMENT,
+  AccountName varchar(255) NOT NULL,
+  CurrencyType varchar(5) NOT NULL,
+  AccountNumber char(10) NOT NULL,
+  Balance double NOT NULL DEFAULT '0',
+  UserId int NOT NULL,
+  PRIMARY KEY (AccoundId),
+  FOREIGN KEY (UserId) REFERENCES user (UserId)
+);
+
+CREATE TABLE Transaction (
+  TransactionId int NOT NULL AUTO_INCREMENT,
+  SenderAccountId int NOT NULL,
+  ReceiverAccountId int NOT NULL,
+  TransactionDate datetime NOT NULL,
+  Amount double NOT NULL,
+  PRIMARY KEY (TransactionId),
+  FOREIGN KEY (SenderAccountId) REFERENCES Account (AccoundId),
+  FOREIGN KEY (ReceiverAccountId) REFERENCES Account (AccoundId)
+);
+
