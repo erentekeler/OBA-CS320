5343d372b41fb12f5e7d15f55a1cbd03fa7e50f6
Ceyhun Topal <ceyhun.topal@ozu.edu.tr>
2022-12-21 19:41:23 +0300

    Added status variable (0 if received, 1 if sent)

--- Model/Transaction.py
+++ Model/Transaction.py
@@ -1,10 +1,10 @@
 class Transaction:
 
-    def __init__(self, toIBAN, amount):
+    def __init__(self, fromIBAN, toIBAN, amount, status):
         self.amount = amount
+        self.fromIBAN = fromIBAN
         self.toIBAN = toIBAN
+        self.status = status #0 if received, 1 if sent
 
     def updateDatabase(self):
         print("Transaction.updateDatabase()")
-
-
