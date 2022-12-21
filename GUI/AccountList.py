af07cb3b3c190a9cc4574990bbff881ab946a9d8
Metin Köker <metin.koker@ozu.edu.tr>
2022-12-21 19:59:48 +0300

    Account List Page added

---
+++ GUI/AccountList.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg 
+
+sg.theme('LightGreen6')   # select theme     
+temp_list = ['Account1','Account2','Account3']
+
+layout = [[sg.Text('Select the Account: ',size = (17, 1), font=('Arial',12,'bold')), sg.Combo(temp_list, size = 40)],
+          [sg.Button('Enter', size = (8, 1),font=("Arial",12,'bold'), pad=(10,10)),sg.Button('Go Back', size = (8, 1),font=("Arial",12,'bold'), pad=(10,10), button_color='Dark Grey')]]
+
+
+                       
+
+window = sg.Window('Account List Page - OBA', layout,input, size=(300,150),element_justification='c')    
+
+window.read()
