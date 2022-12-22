399fac3efccc2f2041404b31e7c65208e7265660
NFA26 <fatih.altunel@ozu.edu.tr>
2022-12-22 16:47:44 +0300

    launcher added Loginpage without model

---
+++ Launcher.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import GUI
+import Controller
+from Controller import BankingController
+from Controller import LoginPageController
+
+a=LoginPageController.LoginPageController()
+a.openPage()
