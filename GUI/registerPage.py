1c8e2950b9650ad0d1b872fdf528d61649b22d80
Beyzanur Aydın <beyzanur.aydin@ozu.edu.tr>
2022-12-22 18:21:35 +0300

    key names are changed

--- GUI/registerPage.py
+++ GUI/registerPage.py
@@ -4,10 +4,10 @@ sg.theme('DarkGreen')   # select theme
 
 layout = [
         [sg.Text('Register Your Account',size = (200, 1), font=('Arial',20,'bold'),justification = 'center')],   
-        [sg.Text('Name: ',font=('Arial',12,'bold'),size =(12, 1)), sg.InputText(key='ID', size=(20,1))],   
-        [sg.Text('Surname: ',font=("Arial",12,'bold'),size =(12, 1)), sg.InputText(key='ID', size=(20,1))], 
+        [sg.Text('Name: ',font=('Arial',12,'bold'),size =(12, 1)), sg.InputText(key='Name', size=(20,1))],   
+        [sg.Text('Surname: ',font=("Arial",12,'bold'),size =(12, 1)), sg.InputText(key='Surname', size=(20,1))], 
         [sg.Text('Password:',font=("Arial",12,'bold'),size =(12, 1)), sg.InputText(key='Password', size=(20,1))],
-        [sg.Text('Your Given ID is:',font=("Arial",12,'bold'),size =(14, 1)), sg.InputText(key='ID', size=(20,1), background_color =('grey'),pad=(50))],     
+        [sg.Text('Your Given ID is:',font=("Arial",12,'bold'),size =(14, 1)), sg.InputText(key='givenID', size=(20,1), background_color =('grey'),pad=(50))],     
         [sg.Button('Create Account', size = (25, 1),font=("Arial",12,'bold'), pad=(25,25))],
         [sg.Text('Already have an account?'), sg.Button('SIGN IN',font=("Arial",8,'bold'))]
         ]
