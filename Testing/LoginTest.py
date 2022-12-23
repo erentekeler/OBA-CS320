d5e702a17b5d7f33a01e225e91ac0754377f4c8f
Ceyhun Topal <ceyhun.topal@ozu.edu.tr>
2022-12-24 01:50:24 +0300

    Added Testing Module

--- Testing/LoginTest.py
+++ Testing/LoginTest.py
@@ -1,15 +1,16 @@
 import unittest
 
+
 class LoginTest(unittest.TestCase):
 
     def test_already_existing_user_register(self):
-
+        pass
 
     def test_new_user_register(self):
-
+        pass
 
     def test_unmatching_user_inputs(self):
-
+        pass
 
     def test_account_listing(self):
-
+        pass
