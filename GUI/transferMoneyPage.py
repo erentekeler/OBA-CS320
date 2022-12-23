1ca2879ed95cbded9056a84aa483b61842cc0596
Metin Köker <metin.koker@ozu.edu.tr>
2022-12-23 13:56:52 +0300

    Receiver Account attributes added

--- GUI/transferMoneyPage.py
+++ GUI/transferMoneyPage.py
@@ -4,14 +4,17 @@ sg.theme('DarkGreen')   # select theme
 temp_list = ['a','b','c']
 temp_list2 = ['d','e','f']
 maximumAmount = 0
+receiverName = 'receiverName1'
 
 layout = [
-        [sg.Text('Select the Sender Account : ',size = (21, 1), font=('Arial',12,'bold')), sg.Combo(temp_list, size = 10),sg.Text('',size = (10,1), font=('Arial',12,'bold')),sg.Text('Select the Receiver Account : ',size = (22, 1), font=('Arial',12,'bold')), sg.Combo(temp_list, size = 10)],
-        [sg.Text('The Maximum amount can be send:  '+str(maximumAmount),size = (50, 1), font=('Arial',12,'bold'))],
+        [sg.Text('Select the Sender Account : ',size = (21, 1), font=('Arial',12,'bold')), sg.Combo(temp_list, size = 10),sg.Text('',size = (8,1), font=('Arial',12,'bold')),sg.Text('Select the Receiver Account ID: ',size = (24, 1), font=('Arial',12,'bold')), sg.Combo(temp_list, size = 10)],
+
+
+        [sg.Text('The Maximum amount can be send:  '+str(maximumAmount),size = (50, 1), font=('Arial',12,'bold')),sg.Button('Verify', size = (10, 1),font=("Arial",12,'bold'), pad=(25,1), button_color='Dark Green')],
         #[sg.Text('Select the Account to Send Money to: ',size = (30, 1), font=('Arial',12,'bold')), sg.Combo(temp_list2, size = 40)],
-        [sg.Text('Amount to be Sent: ',size = (21, 1), font=('Arial',12,'bold')), sg.InputText(key='amount', size=(12,1))],
+        [sg.Text('Amount to be Sent: ',size = (21, 1), font=('Arial',12,'bold')), sg.InputText(key='amount', size=(12,1)),sg.Text('',size = (8, 1), font=('Arial',12,'bold')),sg.Text('Receiver\'s Name : '+receiverName,size = (30, 1), font=('Arial',12,'bold'))],
         [sg.Button('TRANSFER', size = (10, 1),font=("Arial",20,'bold'), pad=(25,25), button_color='Dark Green')],
-        [sg.Button('Logout', size = (10, 1),font=("Arial",12,'bold'), pad=(25,25), button_color='Dark Grey')]
+        [sg.Button('Go Back', size = (10, 1),font=("Arial",12,'bold'), pad=(25,25), button_color='Dark Grey')]
        
         ]
                        
