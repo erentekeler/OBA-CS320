75c7c39238a90d923b8bbd4e609cf05c549617f1
Eren Tekeler <eren.tekeler@ozu.edu.tr>
2022-12-24 19:13:29 +0300

    Functionality done!

--- Controller/TransferMoneyPageController.py
+++ Controller/TransferMoneyPageController.py
@@ -41,16 +41,19 @@ class TransferMoneyPageController():
                 if(self.acc.getUserFullNameFromAccountId(values['receiverID'])):
                     view['reciever'].update('Receiver\'s Name : '+str(self.acc.getUserFullNameFromAccountId(values['receiverID'])))
                 elif(self.acc.getUserFullNameFromAccountId(values['receiverID'])==False):
+                    view['reciever'].update('Receiver\'s Name : ' + str("Not Found!"))
                     sg.popup('Fail','There is no matching account ID')
             elif(events=='TRANSFER'):
                 temp = values['AccNames']
                 if(self.acc.getUserFullNameFromAccountId(values['receiverID'])==False):
                     sg.popup('Transfer Failed','There is no matching account ID')
-                elif(float(values['amount'])<=self.ab.getAccountBalanceFromAccountName(values['AccNames'],self.customer)[0]):
-                    self.transaction.makeTransaction(values["AccNames"],self.customer,values['receiverID'],values['amount'])
+                elif(float(values['amount'])<=self.ab.getAccountBalanceFromAccountName(values['AccNames'],self.customer)[0] and self.transaction.makeTransaction(values["AccNames"],self.customer,values['receiverID'],values['amount'])):
                     sg.popup('Transfer Succsessful','Your new balance is ' + str(self.ab.getAccountBalanceFromAccountName(temp,self.customer)[0] - float(values['amount'])) + ' Thank you for choosing OBA')
                     break
-                elif(float(values['amount']>self.ab.getAccountBalanceFromAccountName(values['AccNames'],self.customer)[0])):
+                elif(self.transaction.makeTransaction(values["AccNames"],self.customer,values['receiverID'],values['amount']) == False):
+                    print("imdat")
+                    sg.popup('Transfer Failed', 'Currency type should match!')
+                elif(float(values['amount'])>self.ab.getAccountBalanceFromAccountName(values['AccNames'],self.customer)[0]):
                     sg.popup('Transfer Fail', 'You don\'t have enough money')
 
             elif(events=='Go Back'):
