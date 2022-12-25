25e300894e7a38572f923a348583a5467f967265
NFA26 <fatih.altunel@ozu.edu.tr>
2022-12-25 21:31:21 +0300

    Test 008 final

--- Testing/TransactionTest.py
+++ Testing/TransactionTest.py
@@ -1,15 +1,17 @@
 import unittest
 import sys
 import os
+import datetime
 os.path.normpath(os.getcwd() + os.sep + os.pardir)
 sys.path.insert(1, os.getcwd())
-from Data import TransactionRepository as tr
+from Model import Transaction as tr
 
+class TransactionTest(unittest.TestCase):
 
-def test_008(self):
-        temp = tr.TransactionRepository()
-        self.assertTrue(temp.getReceiverAccountTransactions(1)==['1','1000005','1000004','2022-12-25','20', 'TL'])
-        self.assertTrue(temp.getSenderAccountTransactions(4)==['2','1000000','1000002','2022-12-25','10', 'TL'])
+        def test_008(self):
+                temp = tr.Transaction()
+                self.assertTrue(temp.filterTransactions(3,1)==[('TL hesabım',10.0,'TL',datetime.date(2022,12,25))])
+                self.assertTrue(temp.filterTransactions(4,2)==[('TL hesabım',10.0,'TL',datetime.date(2022,12,25))])
 
 if __name__ == '__main__':
     unittest.main()
