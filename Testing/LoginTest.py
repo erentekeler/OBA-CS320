eefe37f2a27e610dd9c937c4f708231bfa14cf04
NFA26 <fatih.altunel@ozu.edu.tr>
2022-12-25 18:32:24 +0300

    test001 draft

--- Testing/LoginTest.py
+++ Testing/LoginTest.py
@@ -1,10 +1,19 @@
 import unittest
-
-
+import sys
+import os
+os.path.normpath(os.getcwd() + os.sep + os.pardir)
+sys.path.insert(1, os.getcwd())
+from Model import Customer as cs
+from Data import UserRepository as Ur
 class LoginTest(unittest.TestCase):
 
     def test_001_1(self):
-        pass
+        temp=cs.Customer()
+        repo=Ur.UserRepository()
+        #ExistingUser '11335678910'
+        self.assertTrue(temp.registerCheck('11335678910')==str(8))
+        self.assertIsNotNone(repo.getUserIdFromIdentityNoV2('11335678910'))
+        
 
     def test_001_2(self):
         pass
@@ -15,3 +24,6 @@ class LoginTest(unittest.TestCase):
     def test_003(self):
         pass
 
+
+if __name__ == '__main__':
+    unittest.main()
