d052c656dd5f58acbe4551d02308886a79c1599b
beyzaydiin <beyzanur.aydin@ozu.edu.tr>
2022-12-25 20:48:28 +0300

    tests 2 -3 are added

--- Testing/LoginTest.py
+++ Testing/LoginTest.py
@@ -4,6 +4,7 @@ import os
 os.path.normpath(os.getcwd() + os.sep + os.pardir)
 sys.path.insert(1, os.getcwd())
 from Model import Customer as cs
+from Data import AccountRepository as acr
 from Data import UserRepository as Ur
 class LoginTest(unittest.TestCase):
 
@@ -16,13 +17,31 @@ class LoginTest(unittest.TestCase):
         
 
     def test_001_2(self):
-        pass
+        temp=cs.Customer()
+        repo=Ur.UserRepository()
+        #new id 98979695123
+        self.assertTrue(temp.registerCheck('98979695123'))
+        self.assertIsNone(repo.getUserIdFromIdentityNoV2('98979695123'))
 
     def test_002(self):
-        pass
+        temp = cs.Customer()
+        repo=Ur.UserRepository() 
+        #ExistingUser '11335678910'
+        self.assertTrue(temp.loginCheck('11335678910','c1234')== False)
+        self.assertTrue(temp.loginCheck('01335678910','lc1234')== False)
+        self.assertTrue(temp.loginCheck('11335678910','lc1234')== True)
+
+
 
     def test_003(self):
-        pass
+        temp = acr.AccountRepository()
+        self.assertTrue(temp.getAccountNamesOfUser(4)==['TL hesabım','Dolar hesabım'])
+        self.assertTrue(temp.getCurrencyFromAccountName('TL hesabım',4)[0]=='TL')
+        self.assertTrue(temp.getCurrencyFromAccountName('Dolar hesabım',4)[0]=='USD')
+        self.assertTrue(temp.getAccountIdFromAccountName('TL hesabım',4)[0]==1000000)
+        self.assertTrue(temp.getAccountIdFromAccountName('Dolar hesabım',4)[0]==1000001)
+
+       
 
 
 if __name__ == '__main__':
